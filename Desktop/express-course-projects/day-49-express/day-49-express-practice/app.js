f7519a66126cff406767b2424b7233a0b8d9dd0c
Wendy238 <wendy.Kinyuy@icloud.com>
2026-03-20 19:10:01 +0100

    rendering dynamic content with ejs template, outputing repeated content with EJS,including partial content

--- Desktop/express-course-projects/day-49-express/day-49-express-practice/app.js
+++ Desktop/express-course-projects/day-49-express/day-49-express-practice/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
@@ -11,42 +14,44 @@ app.get('/', function (req, res) {
     res.send('<h1>Hello World</h1>');
 });
 
-app.get('/restaurants', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
-    res.sendFile(htmlFilePath);
+app.get('/restaurants', (req, res) => {
+     const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+  res.render('restaurants', {numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants});
 });
 
+
 app.get('/recommend', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'recommend.html');
-    res.sendFile(htmlFilePath);
+    res.render('recommend');
 });
 
 app.post('/recommend', function (req, res) {
-    const restaurants = req.body;
+    const restaurant = req.body;
     const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
     const fileData = fs.readFileSync(filePath);
     const storedRestaurants = JSON.parse(fileData);
 
-    storedRestaurants.push(restaurants);
+    storedRestaurants.push(restaurant);
+
     fs.writeFileSync(filePath, JSON.stringify(storedRestaurants, null, 2));
 
     res.redirect('/confirm');
 });
 
 app.get('/index', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'index.html');
-    res.sendFile(htmlFilePath);
+    res.render('index');
 });
 
 app.get('/confirm', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'confirm.html');
-    res.sendFile(htmlFilePath);
+    res.render('confirm');
 });
 
 app.get('/about', function (req, res) {
-    const htmlFilePath = path.join(__dirname, 'views', 'about.html');
-    res.sendFile(htmlFilePath);
+    res.render('about');
 });
 
 app.listen(3001, function () {
